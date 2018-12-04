2edd82744cd3e70c9cfecdf6ce8c237f5f074ff5
yinwanqi <yinw0005@e.ntu.edu.sg>
2018-12-04 21:08:58 +0900

    spmv_v3

--- 03spmv/include/vec_norm2_parallel.cc
+++ 03spmv/include/vec_norm2_parallel.cc
@@ -9,19 +9,22 @@
     @returns the square norm of v (v[0]^2 + ... + v[n-1]^2)
 */
 static real vec_norm2_parallel(vec_t v) {
-  fprintf(stderr,
+  /*fprintf(stderr,
           "*************************************************************\n"
           "%s:%d:vec_norm2_parallel:\n"
           "write a code that computes square norm of a vector v\n"
           "using parallel for + reduction.\n"
           "*************************************************************\n",
           __FILE__, __LINE__);
-  exit(1);
+  exit(1);*/
 
   real s = 0.0;
   real * x = v.elems;
   idx_t n = v.n;
+  #pragma omp declare reduction (sp: real: s_add (&omp_out, &opm_in)) initializer(s_init(&omp_priv))
+  #pragma omp for reduction (sp : s)
   for (idx_t i = 0; i < n; i++) {
+    #pragma omp parallel reduction()
     s += x[i] * x[i];
   }
   return s;
