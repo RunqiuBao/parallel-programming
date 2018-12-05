e25dfd449d851e14ddf08a9ca2284a4af0bc9e5a
yinwanqi <yinw0005@e.ntu.edu.sg>
2018-12-05 18:32:11 +0900

    coo_cuda_v1

--- 03spmv/include/spmv_coo_cuda.cc
+++ 03spmv/include/spmv_coo_cuda.cc
@@ -32,17 +32,35 @@ __global__ void spmv_coo_dev(sparse_t A, vec_t vx, vec_t vy) {
     @returns 1 if succeed, 0 if failed
 */
 static int spmv_coo_cuda(sparse_t A, vec_t vx, vec_t vy) {
-  fprintf(stderr,
+  /*fprintf(stderr,
           "*************************************************************\n"
           "%s:%d:spmv_coo_cuda:\n"
           "write a code that performs SPMV for COO format in parallel\n"
           "using CUDA.\n"
           "*************************************************************\n",
           __FILE__, __LINE__);
-  exit(1);
+  exit(1);*/
+  idx_t M = A.M;
+  idx_t nnz = A.nnz;
+  coo_elem_t * elems = A.coo.elems;
+  real * x = vx.elems;
+  real * y = vy.elems;
+  for (idx_t i = 0; i < M; i++) {
+    y[i] = 0.0;
+  }
+
+  for (idx_t k = 0; k < nnz; k++) {
+    coo_elem_t * e = elems + k;
+    idx_t i = e->i;
+    idx_t j = e->j;
+    real  a = e->a;
+    real ax = a * x[j];
+    //y[i] += ax;
+    atomicAdd(y[i], x);
+  }
 
   /* this is a serial code for your reference */
-  idx_t M = A.M;
+  /*idx_t M = A.M;
   idx_t nnz = A.nnz;
   coo_elem_t * elems = A.coo.elems;
   real * x = vx.elems;
@@ -57,6 +75,6 @@ static int spmv_coo_cuda(sparse_t A, vec_t vx, vec_t vy) {
     real  a = e->a;
     real ax = a * x[j];
     y[i] += ax;
-  }
+  }*/
   return 1;
 }
