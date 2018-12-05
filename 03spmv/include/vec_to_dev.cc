5b2d59e840e5cd78699006fa8ddbc7a79864f31c
yinwanqi <yinw0005@e.ntu.edu.sg>
2018-12-05 15:41:52 +0900

    v_malloc_v1

--- 03spmv/include/vec_to_dev.cc
+++ 03spmv/include/vec_to_dev.cc
@@ -11,12 +11,45 @@
     @sa sparse_to_dev
 */
 static int vec_to_dev(vec_t& v) {
-  fprintf(stderr,
+  /*fprintf(stderr,
           "*************************************************************\n"
           "%s:%d:vec_to_dev:\n"
           "write a code that copies the elements of v to the device.\n"
           "use dev_malloc and to_dev utility functions in cuda_util.h\n"
           "*************************************************************\n",
-          __FILE__, __LINE__);
+          __FILE__, __LINE__);*/
+
+    //sparse_t dst; //address on device
+      fprintf(stderr,
+              "*************************************************************\n"
+              "v allocating\n"
+              "*************************************************************\n",
+              __FILE__, __LINE__);
+
+    //first get the size of A
+    size_t sz = sizeof(v.elems);
+    fprintf(stderr,
+              "*************************************************************\n"
+              "v got the size\n"
+              "*************************************************************\n",
+              __FILE__, __LINE__);
+
+    //allocate the dev memory
+    void * dst = dev_malloc(sz);
+    v.elems_dev =(real *)dst;
+    fprintf(stderr,
+              "*************************************************************\n"
+              "v assigned the size\n"
+              "*************************************************************\n",
+              __FILE__, __LINE__);
+
+    //copy the data to device
+    to_dev( (void *)v.elems_dev, (void *)v.elems, sz);
+
+    fprintf(stderr,
+            "*************************************************************\n"
+            "v allocated\n"
+            "*************************************************************\n",
+            __FILE__, __LINE__);
   return 1;
 }
