23815c4c475aa5ede60a535c41a7d2d151c58941
runqiu <runqiu.bao@tum.de>
2018-12-07 16:12:30 +0900

    forwanqi

--- 03spmv/include/spmv_csr_cuda.cc
+++ 03spmv/include/spmv_csr_cuda.cc
@@ -13,36 +13,6 @@
 
 /*runqiu:kernel*/
 __global__
-void vy_to_zero(sparse_t A, vec_t vy){
-
-  int kidcc = blockDim.x * blockIdx.x + threadIdx.x;//runqiu:thread id
-  
-  if(kidcc<A.M){
-    //real temp=0.0;
-    *(vy.elems_dev+kidcc)=0.0;
-  }
-  
-}
-
-__global__
-void spmv_coo_dev(idx_t *ia, sparse_t A, vec_t vx, vec_t vy){
-
-  int kidco = blockDim.x * blockIdx.x + threadIdx.x;//runqiu:thread id
-  //idx_t * row_start = A.csr.row_start_dev;
-  real * x = vx.elems_dev;
-  real * y = vy.elems_dev; 
-  //csr_elem_t * elems = A.csr.elems_dev;
-  if(kidco<A.nnz){
-    csr_elem_t * e = A.csr.elems_dev + kidco;
-    idx_t j=e->j;
-    real a=e->a;
-    atomicAdd(&y[ia[kidco]], a * x[j]);
-  }
-  //printf("coo one step!");
-}
-
-
-__global__
 void spmv_csr_dev(sparse_t A, vec_t vx, vec_t vy){
 
   int kid = blockDim.x * blockIdx.x + threadIdx.x;//runqiu:thread id
@@ -83,49 +53,17 @@ static int spmv_csr_cuda(sparse_t A, vec_t vx, vec_t vy) {
   {  
    // int bs = 256;
    // int nb = (A.nnz + bs - 1) / bs;
-    if(A.M>50){
-      int bs = 256;
-      int nb = (A.M + bs - 1) / bs;
-      check_launch_error((vy_to_zero<<<nb,bs>>>(A, vy)));
-      check_launch_error((spmv_csr_dev<<<nb,bs>>>(A, vx, vy)));
-      printf("spmv-csr succeed!");   
-      //dev_free((void*)A.csr.elems_dev);
-      //dev_free((void*)vx.elems_dev);
-      //dev_free((void*)vy.elems_dev);
-    }
-    else 
-    {
-      int bs2 = 256;
-      int bs=256;
-      int nb=(A.M + bs - 1) / bs;
-      int nb2 = (A.nnz + bs2 - 1) / bs2;
-      idx_t *ia;
-      ia=(idx_t*)malloc(A.nnz*sizeof(idx_t));
-      //check_launch_error((csr_to_coo<<<nb2,bs2>>>(ia, A, vx, vy)));
-      int k=0;
-      for(int i=0;i<A.nnz;i++){
-        if(k<(A.M-1))
-          if(i<A.csr.row_start[k+1])
-            ia[i]=i;
-          else
-            k++;
-        else
-          ia[i]=A.M;
-      }
-      printf("ia succeed!");
-      idx_t *ia_dev;
-      ia_dev=(idx_t*)dev_malloc(A.nnz*sizeof(idx_t));
-      to_dev((void*)ia_dev, (void*)ia, sizeof(idx_t));
-      printf("finish build coo!");
-      check_launch_error((vy_to_zero<<<nb,bs>>>(A, vy)));
-      check_launch_error((spmv_coo_dev<<<nb2,bs2>>>(ia_dev, A, vx, vy)));
-      printf("spmv-csr succeed!");
-      //dev_free((void*)A.csr.elems_dev);
-      //dev_free((void*)ia);
-      //dev_free((void*)vx.elems_dev);
-    }
-    to_host((void*)vy.elems, (void*)vy.elems_dev, sizeof(real)*A.M); 
-    printf("receive vy succeed!");
+   int bs = 256;
+   int nb = (A.M + bs - 1) / bs;
+   check_launch_error((vy_to_zero<<<nb,bs>>>(A, vy)));
+   check_launch_error((spmv_csr_dev<<<nb,bs>>>(A, vx, vy)));
+   printf("spmv-csr succeed!");   
+   //dev_free((void*)A.csr.elems_dev);
+   //dev_free((void*)vx.elems_dev);
+   //dev_free((void*)vy.elems_dev);
+    
+   to_host((void*)vy.elems, (void*)vy.elems_dev, sizeof(real)*A.M); 
+   printf("receive vy succeed!");
   
     //check_api_error((cudaMemcpy(vy.elems, vy.elems_dev, sizeof(real)*A.M, cudaMemcpyDeviceToHost)));
   }
